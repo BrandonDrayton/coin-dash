e7e663318abb479ba16bd9edbe1650941548a060
BrandonDrayton <brandondrayton@gmail.com>
2023-03-30 10:19:06 -0400

    homepage crypto fix

--- src/components/CryptoCurrencies10.jsx
+++ src/components/CryptoCurrencies10.jsx
@@ -9,7 +9,7 @@ import Add from './Add'
 export const Cryptocurrencies10 = () => {
     const [cryptoArray, setCryptoArray] = useState([])
     const getCryptoArray = () => {
-        fetch('https://api.coingecko.com/api/v3/coins')
+        fetch('https://api.coingecko.com/api/v3/coins/')
             .then(res => res.json())
             .then(coins => {
                 setCryptoArray(coins)
@@ -24,7 +24,7 @@ export const Cryptocurrencies10 = () => {
             <Row gutter={[32, 32]} className='crypto-card-container'>
                 {cryptoArray.filter((e) => e.market_data.market_cap_rank < 11).map((coin) => (
                     <Col xs={24} sm={12} lg={6} key={coin.localization.en}>
-                        <Link to={`/crypto/${coin.id}`}>
+                        <Link to={`crypto/${coin.id}`}>
                             <Card
                                 title={`${coin.market_data.market_cap_rank}. ${coin.name}`}
                                 extra={<img className='crypto-image' src={coin.image.thumb} alt='crypto' />}
